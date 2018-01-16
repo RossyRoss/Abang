1bf426d079868872a34a8711322c85cadd517946
abangcapstone <infozojt@gmail.com>
2018-01-17 04:13:47 +0800

    ross

---
+++ app/src/main/java/capstone/abang/com/Car_Renter/Car_Renter.java
@@ -0,0 +1,55 @@
+package capstone.abang.com.Car_Renter;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import capstone.abang.com.R;
+import capstone.abang.com.Utils.BottomNavigationViewHelper;
+
+public class Car_Renter extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_favourites:
+
+                    return true;
+                case R.id.navigation_map:
+
+                    return true;
+                case R.id.navigation_history:
+
+                    return true;
+                case R.id.navigation_profile:
+
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_renter);
+
+        BottomNavigationView navigation =  findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
